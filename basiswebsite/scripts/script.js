211f4601e4927af4aae30fa1245e5c8c96a0940d
noabos <74159609+noabos@users.noreply.github.com>
2020-11-23 16:57:30 +0100

    Add files via upload

--- basiswebsite/scripts/script.js
+++ basiswebsite/scripts/script.js
@@ -1,42 +1,38 @@
-// define all UI variable
+// NAVIGATIE HAMBURGER
 const navToggler = document.querySelector('.nav-toggler');
 const navMenu = document.querySelector('nav ul');
 const navLinks = document.querySelectorAll('nav a');
 
-// load all event listners
 allEventListners();
 
-// functions of all event listners
 function allEventListners() {
-  // toggler icon click event
+  // als je klikt popt hij op
   navToggler.addEventListener('click', togglerClick);
-  // nav links click event
   navLinks.forEach( elem => elem.addEventListener('click', navLinkClick));
 }
 
-// togglerClick function
 function togglerClick() {
   navToggler.classList.toggle('toggler-open');
   navMenu.classList.toggle('open');
 }
 
-// navLinkClick function
 function navLinkClick() {
   if(navMenu.classList.contains('open')) {
     navToggler.click();
   }
 }
 
-
 //bron : https://www.javascripttutorial.net/javascript-dom/javascript-queryselector///
 
+
+
+// SLIDER BANNER
 var imageSlides = document.getElementsByClassName('imageSlides');
 var circles = document.getElementsByClassName('circle');
 var leftArrow = document.getElementById('leftArrow');
 var rightArrow = document.getElementById('rightArrow');
 var counter = 0;
 
-// HIDE ALL IMAGES FUNCTION
 function hideImages() {
   for (var i = 0; i < imageSlides.length; i++) {
     imageSlides[i].classList.remove('visible');
@@ -50,7 +46,6 @@ function removeDots() {
   }
 }
 
-// SINGLE IMAGE LOOP/CIRCLES FUNCTION
 function imageLoop() {
   var currentImage = imageSlides[counter];
   var currentDot = circles[counter];
@@ -60,7 +55,7 @@ function imageLoop() {
   counter++;
 }
 
-// LEFT & RIGHT ARROW FUNCTION & CLICK EVENT LISTENERS
+// pijltjes links en rechts
 function arrowClick(e) {
   var target = e.target;
   if (target == leftArrow) {
@@ -97,7 +92,7 @@ leftArrow.addEventListener('click', arrowClick);
 rightArrow.addEventListener('click', arrowClick);
 
 
-// IMAGE SLIDE FUNCTION
+// Na zoveel secondes verplaatst hij automatisch
 function slideshow() {
   if (counter < imageSlides.length) {
     imageLoop();
@@ -108,6 +103,5 @@ function slideshow() {
   }
 }
 
-// SHOW FIRST IMAGE, & THEN SET & CALL SLIDE INTERVAL
 setTimeout(slideshow, 1000);
 var imageSlideshowInterval = setInterval(slideshow, 10000);
