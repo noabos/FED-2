0b266a0ae2fca946517027c673e79a66729dc929
noabos <74159609+noabos@users.noreply.github.com>
2021-01-19 11:48:56 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,88 +1,69 @@
 // NAVIGATIE HAMBURGER
-const navToggler = document.querySelector('.nav-toggler');
-const navMenu = document.querySelector('nav ul');
-const navLinks = document.querySelectorAll('nav a');
-
-allEventListners();
-
-function allEventListners() {
-
-    // als je klikt popt hij op
-    navToggler.addEventListener('click', togglerClick);
-    navLinks.forEach(elem => elem.addEventListener('click', navLinkClick));
-}
+var navToggler = document.querySelector('.nav-toggler');
+var navMenu = document.querySelector('nav ul');
 
 function togglerClick() {
-    navToggler.classList.toggle('toggler-open');
-    navMenu.classList.toggle('open');
+    navToggler.classList.toggle('toggler-open'); // dit maakt het vierkantje wit
+    navMenu.classList.toggle('open'); // dit maakt 'm open
 }
 
-function navLinkClick() {
-    if (navMenu.classList.contains('open')) {
-        navToggler.click();
-    }
-}
-
+// als je klikt popt hij op
+navToggler.addEventListener('click', togglerClick); // Eventlisitener voor de toggler
 
 
 
 // GALLERIJ
 
-const listItems = document.querySelectorAll('.filteroptie');
-const allImages = document.querySelectorAll('.filter');
+var listItems = document.querySelectorAll('.filteroptie');
+var allImages = document.querySelectorAll('.filter');
 
-function toggleActiveClass(active) {
+function toggleActiveClass(active) {  //eerst overal active weghalen, waar je op klikt wordt "active" en die geeft hij de class "active"
     listItems.forEach(item => {
         item.classList.remove('active');
     })
-    active.classList.add('active');
+    active.classList.add('active'); //hier verandert hij de vormgeving
 }
 
 function toggleImages(dataClass) {
 
-    for (let i = 0; i < allImages.length; i++)
+    for (let i = 0; i < allImages.length; i++)  // let is een var die je gebruikt voor een bepaalde functie, let is bepaald nummer wordt meteen weer vergeten, bij var is dat niet zo
 
-        if (allImages[i].dataset.class === dataClass) {
-
-            if (allImages[i].classList.contains('visible')) {} else {
+        if (allImages[i].dataset.class === dataClass) {   // als ie zichtbaar is dan laat ie t zien en anders niet
                 allImages[i].classList.add('visible');
                 allImages[i].classList.remove('unvisible');
-            }
 
-        } else {
 
-            if (allImages[i].classList.contains('visible')) {
+        } else { // als ie zichtbaar is dan laat ie t zien en anders niet
                 allImages[i].classList.add('unvisible');
                 allImages[i].classList.remove('visible');
-            }
         }
 }
 
-for (let i = 0; i < listItems.length; i++) {
-    listItems[i].addEventListener('click', function () {
-        toggleActiveClass(listItems[i]);
-        toggleImages(listItems[i].dataset.class);
+for (let x = 0; x < listItems.length; x++) { // js loop // als ie zichtbaar is dan laat ie t zien en anders niet
+    listItems[x].addEventListener('click', function () {
+        toggleActiveClass(listItems[x]); //hier kijkt hij naar welke element op geklikt is, dat geeft hij mee aan de functie.
+        toggleImages(listItems[x].dataset.class); // DATASET.CLASS - Het element waar je op hebt geklikt, kijkt hij naar welke classes hier op zitten. Hij geeft de class van het element weer.
     });
 }
 
 //SLIDER
 
-var slideIndex = 0;
+var slideIndex = 0;  //Hij begint op 0, daarna gaat hij optellen.
 showSlides();
 
 function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
 
-  setTimeout(showSlides, 3000);
+  var slides = document.getElementsByClassName("mySlides");
+
+  for (let i = 0; i < slides.length; i++) { //elke keer komt er 1 slide bij, dit maakt alles onzichtbaar, daarna wordt er 1 zichtbaar
+    slides[i].style.display = "none"; //verdwijnen andere slides
+  }
+
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1} //houdt bij hoeveel slides hij heeft gehad, als hij groter is, dan start hij weer opnieuw
+
+  slides[slideIndex-1].style.display = "block"; //1 voor 1 en dan worden ze weer zichtbaar
+
+  setTimeout(showSlides, 3000); //hoelang voordat de volgende slide komt, 3sec
+
 }
